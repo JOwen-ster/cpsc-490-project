9a8c1da906799ca963dbce30409adcb627401125
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2026-02-05 09:44:39 -0800

    Update auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import GitHubLoginButton from "./components/GitHubLoginButton";
+import GitHubLoginButton from "@/components/GitHubLoginButton";
 
 export default function Home() {
   return (
