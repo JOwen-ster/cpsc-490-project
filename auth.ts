c46f4f8c03c70bc0c97b984fdecefbaa46e3e9e8
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2026-02-05 12:09:13 -0800

    Update auth

--- auth.ts
+++ auth.ts
@@ -1,6 +1,21 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [GitHub],
-})
+    providers: [
+        GitHub({
+            clientId: process.env.AUTH_GITHUB_ID!,
+            clientSecret: process.env.AUTH_GITHUB_SECRET!,
+        }),
+    ],
+
+    callbacks: {
+        async jwt({ token, user, profile }) {
+            if (user && profile) {
+                token.id = profile.id;
+                token.username = profile.login; // username
+            }
+            return token;
+        },
+    },
+});
