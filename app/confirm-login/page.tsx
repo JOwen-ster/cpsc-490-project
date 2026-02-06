3e47f676874972f7088740a13a5fc351fe2cb824
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2026-02-05 16:19:25 -0800

    Update Login Page Component Name

--- app/confirm-login/page.tsx
+++ app/confirm-login/page.tsx
@@ -1,7 +1,7 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
-export default async function AfterLoginPage() {
+export default async function ConfirmLoginPage() {
     const session = await auth();
 
     if ( !session ) redirect("/");
